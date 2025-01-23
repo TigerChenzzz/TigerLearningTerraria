918e11c2df646e664f22c0d98d135b859c784497
TigerChenzzz <1139719265@qq.com>
2025-01-23 09:42:44 +0800

    Update 键位绑定.cs

--- Learning/键位绑定.cs
+++ Learning/键位绑定.cs
@@ -3,12 +3,10 @@ using Terraria.GameInput;
 
 namespace TigerLearning.Learning;
 
-public class 键位绑定
-{
+public class 键位绑定 {
     // 用此类以定义一个键位, 这样就可以在游戏中设置了
     public static ModKeybind ExampleKeybind;
-    public static void 注册键位()
-    {
+    public static void 注册键位() {
         #region params
         Mod mod = default;
         string name = default;
@@ -19,13 +17,24 @@ public class 键位绑定
         // 本地化键值为 Mods.[ModName].Keybinds.[keybind name].DisplayName
     }
 
-    public static void 使用键位()
-    {
+    public static void 使用键位() {
         // 以下实际上都是使用 PlayerInput.Triggers 作检查
         // 一般需要在 ModPlayer.ProcessTriggers 中使用
         Show(ExampleKeybind.Current);       // 检测是否按住
         Show(ExampleKeybind.JustPressed);   // 是否刚刚按下
         Show(ExampleKeybind.JustReleased);  // 是否刚刚抬起
         Show(ExampleKeybind.Old);           // 上一帧是按住
+    }
+
+    public class PlayerKeybindExample : ModPlayer {
+        public static ModKeybind SomeKeybind;
+        public override void Load() {
+            ExampleKeybind = KeybindLoader.RegisterKeybind(ModInstance, "SomeKeybind", Keys.K);
+        }
+        public override void ProcessTriggers(TriggersSet triggersSet) {
+            if (ExampleKeybind.JustPressed) {
+                CombatText.NewText(Main.LocalPlayer.Hitbox, Color.White, "Key pressed!");
+            }
+        }
     }
 }
